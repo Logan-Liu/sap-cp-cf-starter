3b94db2671e815f8bb2defecb9bc1fdccd1075a2
ionescuv <victor.ionescu@msg-systems.com>
2019-04-12 16:33:47 +0300

    added CF deployment manifest files

--- test-env/1_init_cloud.cmd
+++ test-env/1_init_cloud.cmd
@@ -1,3 +1,4 @@
+cf create-service postgresql v9.6-dev my-db
 cf create-service xsuaa application my-uaa -c ../../src/xsuaa/xs-security.json
 cf create-service portal-services site-content my-portal-content -c portal-content/portal.json
 cf create-service portal-services site-host my-portal-host -c portal-content/portal.json

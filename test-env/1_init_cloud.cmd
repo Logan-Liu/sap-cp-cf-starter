09f53431a29b7595e44d050afc53deb30b1dbee5
Gregor Wolf <gregor.wolf@gmail.com>
2019-04-14 17:03:07 +0200

    Correct location of xs-security.json

--- test-env/1_init_cloud.cmd
+++ test-env/1_init_cloud.cmd
@@ -1,5 +1,5 @@
 cf create-service postgresql v9.6-dev my-db
-cf create-service xsuaa application my-uaa -c ../../src/xsuaa/xs-security.json
+cf create-service xsuaa application my-uaa -c ../src/xsuaa/xs-security.json
 cf create-service portal-services site-content my-portal-content -c portal-content/portal.json
 cf create-service portal-services site-host my-portal-host -c portal-content/portal.json
 cf create-service-key my-uaa run-local
